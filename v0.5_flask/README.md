1dc70ad9264418f7fe68db7219b0814a6896d563
Ian Serer <ianvzs@outlook.com>
2020-10-25 17:24:24 +0800

    go wx three

--- v0.5_flask/README.md
+++
@@ -1,2 +0,0 @@
-# wx_third_part_platform
-微信第三方平台
--- v0.5_flask/tests/test_online/test_send_template_msg.py
+++
@@ -1,6 +0,0 @@
-import requests
-url = "http://127.0.0.1:8979/wx_service/send_template_msg"
-data = {
-    "app_id": "waa111asdw43f3b5",
-}
-requests.request("POST", url, json=data)
